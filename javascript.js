9c29ab9fd222b1f2310780c647a491cb6f7298d3
Dariusz Paśko <dariuszpasko@icloud.com>
2017-09-08 22:36:06 +0200

    los

---
+++ javascript.js
@@ -0,0 +1,32 @@
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById('get-joke');
+
+button.addEventListener('click', function(){
+    getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function(){
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+}
+
+
+setTimeout(function() {
+    getJoke();
+}, 2000);
+
+setTimeout(function() {
+    getJoke();
+}, 3000);
+
+
+
